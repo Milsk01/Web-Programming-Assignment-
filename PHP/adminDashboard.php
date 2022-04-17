6be2e9bcea3a756e6b63fb76504f68b85c0c9f4e
JarminJS <jarminjj2@gmail.com>
2022-04-17 19:14:08 +0800

    new files

---
+++ PHP/adminDashboard.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+    <body>
+        <h1>Admin Dashboard</h1>
+
+        <h2>View List of Participants</h2>
+        <button> View Table </button>
+
+        <h2>Find Participants</h2>
+        <!-- This one up to SK how to implement 
+         just in case needed the form below 
+        --> 
+        <form method="POST" action="">
+            <input type="text" name="id" placeholder="ID">
+            <input type="submit" value="Find Participant">
+        </form>
+
+        <h2>Add Category</h2>
+        <form method="POST" action="addCategory.php"> 
+            <input type="text" name="cat" placeholder="E.g.: 2km, 4km, 10km">
+            <input type="submit" value="Add New Category">
+        </form>
+    </body>
+</html>
--- PHP/checkUserExists.php
+++ PHP/checkUserExists.php
@@ -2,21 +2,21 @@
 
     function getUser($conn,$username){
         
-    $sql="SELECT * FROM participants WHERE username=?;";
-	//create prepare stmt
-	$statement = mysqli_stmt_init($conn);
+        $sql="SELECT * FROM participants WHERE username=?;";
+        //create prepare stmt
+        $statement = mysqli_stmt_init($conn);
 
-    if(!mysqli_stmt_prepare($statement, $sql)){
-        echo "Statement Failed"; 
-        return false;
-    } 
-    else {
-        mysqli_stmt_prepare($statement,$sql);
-	    mysqli_stmt_bind_param($statement,"s",$username);
-	    mysqli_stmt_execute($statement);
-	    $result=mysqli_stmt_get_result($statement);
-        return $result;
-    }
+        if(!mysqli_stmt_prepare($statement, $sql)){
+            echo "Statement Failed"; 
+            return false;
+        } 
+        else {
+            mysqli_stmt_prepare($statement,$sql);
+            mysqli_stmt_bind_param($statement,"s",$username);
+            mysqli_stmt_execute($statement);
+            $result=mysqli_stmt_get_result($statement);
+            return $result;
+        }
 	
     }
 ?>
